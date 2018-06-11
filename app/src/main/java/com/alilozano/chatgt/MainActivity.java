a368b80ce14151235d65565acb00a7d44b152101
Ali Lozano <alilozanoc@gmail.com>
2018-06-10 21:14:11 -0500

    firebase list adapter

--- app/src/main/java/com/alilozano/chatgt/MainActivity.java
+++ app/src/main/java/com/alilozano/chatgt/MainActivity.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +25,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int RC_SIGN_IN = 123;
@@ -59,6 +66,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+        DatabaseReference usersRef = database.getReference("usuarios");
+        FirebaseListOptions<Usuario> options = new FirebaseListOptions.Builder<Usuario>()
+                .setLayout(R.layout.contacto_item)
+                .setQuery(usersRef.orderByKey(), Usuario.class)
+                .setLifecycleOwner(this)
+                .build();
+
+        ListView listView = findViewById(R.id.listViewContactos);
+        ListAdapter adapter = new FirebaseListAdapter<Usuario>(options) {
+            @Override
+            protected void populateView(View view, Usuario s, int i) {
+                TextView txtUsuario = (TextView) view.findViewById(R.id.txtNombre);
+                txtUsuario.setText(String.valueOf(s.getNombre()));
+                TextView txtEmail = (TextView) view.findViewById(R.id.txtEmail);
+                txtEmail.setText(String.valueOf(s.getEmail()));
+            }
+        };
+        listView.setAdapter(adapter);
 
 
     }
